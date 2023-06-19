0b4d846c778ba858297f92b45ca7ce8a8bd72461
Santiago <gomezdelatorre15@gmail.com>
2023-06-19 09:13:03 +0200

    Added header and input

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,19 @@
 import styles from'./header.module.css'
+import logo from '../../assets/logo.svg'
+import { AiOutlinePlusCircle } from 'react-icons/ai' 
 
 export function Header() {
     return (
         <header className={styles.header}>
+            <img src={logo} className={styles.logo} />
             <h1>ToDo</h1>
+            <form className={styles.newTaskForm}>
+                <input type="text" placeholder='Add new task' />
+                <button>
+                    Create task
+                    <AiOutlinePlusCircle size={20}/>
+                </button>
+            </form>
         </header>
     )
 }
